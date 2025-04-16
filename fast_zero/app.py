3304e6a7d4268d51bc2675a237ca9d1d5d6251df
George Macedo <geor128@email.com>
2025-04-16 13:08:44 -0300

    implementando banco de dados

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -15,10 +15,10 @@ database = []
 #     return {'message': 'Olá Mundo!'}
 
 
-@app.get('/users/{user_id}')
-def read_root(user_id: int):
-    if user_id == 1:
-        return {'message': 'Olá Joba!'}
+# @app.get('/users/{user_id}')
+# def read_root(user_id: int):
+#     if user_id == 1:
+#         return {'message': 'Olá Joba!'}
 
 
 # @app.get('/', status_code=HTTPStatus.OK, response_class=HTMLResponse)
@@ -121,3 +121,13 @@ def delete_user(user_id: int):
         )
     del database[user_id - 1]
     return {'message': 'User deleted'}
+
+
+@app.get('/users/{user_id}', response_model=UserPublic)
+def read_user__exercicio(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    return database[user_id - 1]
